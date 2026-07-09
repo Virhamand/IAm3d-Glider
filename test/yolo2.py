bbd75bc7bdaff4ccce251171fe7d90b900f1956b
Nhan Nguyen <2005.nnhan@gmail.com>
2026-07-09 13:05:39 -0700

    Remove hard-coded class names

--- test/yolo2.py
+++ test/yolo2.py
@@ -5,53 +5,46 @@ import math
 # model
 model = YOLO('yolov8n.pt')
 
-# object classes
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
-
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
 while True:
     ret, img= cap.read()
+    if not ret: break
+
     results = model(img, stream=True)
 
     for r in results:
         boxes = r.boxes
+
         for box in boxes:
             # bounding box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
             # confidence
             confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
-
+            
             # class name
             cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
+            class_name = model.names[cls]
+
+            print("Confidence --->", confidence)
+            print("Class name -->", class_name)
+
+            # draw box
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
             # object details
+            label = f"{class_name} {confidence}"
             org = [x1, y1]
             font = cv2.FONT_HERSHEY_SIMPLEX
             fontScale = 1
             color = (255, 0, 0)
             thickness = 2
 
-            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+            cv2.putText(img, class_name, org, font, fontScale, color, thickness)
 
 
     cv2.imshow('Webcam', img)
