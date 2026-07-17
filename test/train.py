aea02fa659650f7ddcb1596082a63a657f424529
Detims <2005.nnhan@gmail.com>
2026-07-17 14:00:53 -0700

    update train.py for cuda

--- test/train.py
+++ test/train.py
@@ -1,13 +1,17 @@
 from ultralytics import YOLO
 
-# Load model
-model = YOLO('yolov8n.pt')
+def main():
+    # Load model
+    model = YOLO('yolov8n.pt')
 
-# Training
-results = model.train(
-    data='data.yaml',
-    imgsz=640,
-    epochs=10,
-    batch=8,
-    name='yolov8n_custom'
-)
+    # Training
+    results = model.train(
+        data='data.yaml',
+        imgsz=640,
+        epochs=10,
+        batch=8,
+        name='yolov8n_custom'
+    )
+
+if __name__ == "__main__":
+    main()
